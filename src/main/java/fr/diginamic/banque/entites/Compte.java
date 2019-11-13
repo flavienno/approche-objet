5af158dfa22abbca00550b3c2fada1ff0aa2b107
Flavien Oulhiou-Naudin <flavien.naudin@gmail.com>
2019-11-13 17:31:02 +0100

    v1.4

---
+++ src/main/java/fr/diginamic/banque/entites/Compte.java
@@ -0,0 +1,11 @@
+package fr.diginamic.banque.entites;
+
+public class Compte {
+	int numeroDeCompte;
+	int soldeDuCompte;
+		
+	public Compte(int numeroDeCompte,int soldeDuCompte){
+		this.numeroDeCompte=numeroDeCompte;
+		this.soldeDuCompte=soldeDuCompte;
+	}
+}
---
+++ src/main/java/fr/diginamic/banque/entites/TestBanque.java
@@ -0,0 +1,10 @@
+package fr.diginamic.banque.entites;
+
+public class TestBanque {
+
+	public static void main(String[] args) {
+		Compte c= new Compte(235,1500);
+		System.out.println(c);
+	}
+
+}
