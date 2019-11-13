69b3b6249f0aa31e16ae1342b5840e2db3d8e6b9
Flavien Oulhiou-Naudin <flavien.naudin@gmail.com>
2019-11-13 15:37:05 +0100

    v1.2

--- src/main/java/entites/Personne.java
+++ src/main/java/entites/Personne.java
@@ -15,4 +15,35 @@ public class Personne {
 		this.prenom=prenom;
 		this.adresse_postale=adresse_postale;
 	}
+	//Dans la classe Personne, ajoutez une méthode qui permet d’afficher le nom et le
+	//prénom avec le nom de famille en majuscules. 
+	public void affichePrenomEnMinusculeEtNomEnMajuscule(){
+		System.out.println(this.nom.toUpperCase() );
+		System.out.println(prenom);
+	}
+	//Dans la classe Personne, ajoutez une méthode qui prend un argument en paramètre et
+	//permet de modifier la variable d’instance nom de Personne
+	public void modifierNom (String autreNom){
+		this.nom=autreNom;
+	}
+	//Dans la classe Personne, ajoutez une méthode similaire pour modifier le prénom
+	public void modifierPrenom (String autrePrenom){
+		this.prenom=autrePrenom;
+	}
+	//Dans la classe Personne, ajoutez une méthode similaire pour modifier l’adresse
+	public void modifierAdresse (String autreAdresse_postale){
+		this.adresse_postale=autreAdresse_postale;
+	}
+	//Dans la classe Personne, ajoutez une méthode qui retourne le nom.
+	public String retourneNom (){
+		return nom;
+	}
+	//Dans la classe Personne, ajoutez une méthode qui retourne le prénom.
+	public String retournePrenom (){
+		return prenom;
+	}
+	//Dans la classe Personne, ajoutez une méthode qui retourne l’adresse.
+	public String retourneAdresse (){
+		return adresse_postale;
+	}
 }
--- src/main/java/essais/TestPersonne.java
+++ src/main/java/essais/TestPersonne.java
@@ -5,14 +5,23 @@ import entites.Personne;
 public class TestPersonne {
 
 	public static void main(String[] args) {
-		Personne a= new Personne("naudin","flavien");
+		//Personne a= new Personne("naudin","flavien");
 		
-		Personne b= new Personne("queyrel","audrey");
+		//Personne b= new Personne("queyrel","audrey");
 	
 		Personne c= new Personne("naudin","flavien","125 rue du moulin de semalen");
 		
-		Personne d= new Personne("queyrel","audrey","3 rue de la croix du capitaine");
+		//Personne d= new Personne("queyrel","audrey","3 rue de la croix du capitaine");
 		
+		//a.affichePrenomEnMinusculeEtNomEnMajuscule();
+		//System.out.println(a.retourneNom());
+		//System.out.println(a.retournePrenom());
+		
+		//utiliser la méthode qui permet de modifier l’adresse pour valoriser l’adresse
+		c.modifierAdresse("rue de la paix");
+	
 	}
 
+	
+	
 }
