977554066721cf9ca943ed33f52be47c7c6a6466
Flavien Oulhiou-Naudin <flavien.naudin@gmail.com>
2019-11-13 16:41:56 +0100

    v1.3

---
+++ src/main/java/utils/ConversionNombre.java
@@ -0,0 +1,14 @@
+package utils;
+
+public class ConversionNombre {
+
+	public static void main(String[] args) {
+		String a="123";
+		int i = Integer.parseInt(a);
+		int b= 5;
+		int c=6;
+		System.out.println(Math.max(b, c));      
+
+	}
+
+}
